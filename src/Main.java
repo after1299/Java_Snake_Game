06177550a6d7f26a41f4d8e502ac7d0feb73b75b
PeterHsiao <peter12218@gmail.com>
2023-04-10 22:08:36 +0800

    eat fruit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Main extends JPanel{
+public class Main extends JPanel implements KeyListener{
 	
 	public static final int CELL_SIZE = 20;
 	public static int width = 400;
@@ -16,6 +19,7 @@ public class Main extends JPanel{
 	private Timer t;
 	private int speed = 100;
 	private static String direction;
+	private boolean allowKeyPress;
 	
 	public Main() {
 		snake = new Snake();
@@ -28,6 +32,8 @@ public class Main extends JPanel{
 			}
 		}, 0, speed);
 		direction = "Right";
+		addKeyListener(this	);
+		allowKeyPress = true;
 	}
 	
 	@Override
@@ -35,8 +41,8 @@ public class Main extends JPanel{
 		// System.out.println("We are calling paint component...");
 		// draw a black background
 		g.fillRect(0, 0, width, height);
-		snake.drawSnake(g);
 		fruit.drawFruit(g);
+		snake.drawSnake(g);
 		
 		// remove snake tail and put it in head
 		int snakeX = snake.getSnakeBody().get(0).x;
@@ -51,10 +57,26 @@ public class Main extends JPanel{
 			snakeY += CELL_SIZE;
 		}
 		Node newHead = new Node(snakeX, snakeY);
-		// remover the tail (which node in getSnackBody's index is : snake.getSnakeBody().size() - 1)
-		snake.getSnakeBody().remove(snake.getSnakeBody().size() - 1);
+		// check if the snake eats the fruit
+		if (snake.getSnakeBody().get(0).x == fruit.getX() &&
+				snake.getSnakeBody().get(0).y == fruit.getY()) {
+			// 1. set fruit to a new location
+			fruit.setNewLocation(snake);
+			// 2. draw fruit
+			fruit.drawFruit(g);
+			// 3. score++
+		} else {
+			// if the snake does not eat food before the picture been repaint
+			// remove the tail (which node in getSnackBody's index is : snake.getSnakeBody().size() - 1)
+			// once the snake eats the fruit, program will not remove the tail at this repaint round.
+			snake.getSnakeBody().remove(snake.getSnakeBody().size() - 1);
+		}
 		// add new node(head) for snake
 		snake.getSnakeBody().add(0, newHead);
+		
+		// allow key press after the picture has been repaint
+		allowKeyPress = true;
+		requestFocusInWindow();
 	}
 	
 	@Override
@@ -70,5 +92,34 @@ public class Main extends JPanel{
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		window.setResizable(false);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (allowKeyPress) {
+			if (e.getKeyCode() == 37 && !direction.equals("Right")) {
+				direction = "Left";
+			} else if (e.getKeyCode() == 38 && !direction.equals("Down")) {
+				direction = "Up";
+			} else if (e.getKeyCode() == 39 && !direction.equals("Left")) {
+				direction = "Right";
+			} else if (e.getKeyCode() == 40 && !direction.equals("Up")) {
+				direction = "Down";
+			}
+			allowKeyPress = false;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
